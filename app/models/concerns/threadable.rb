28a024b1da8f5f6c481f3d1662d24d5e3699cca1
Jose Farias <jose@37signals.com>
2024-10-24 11:36:24 -0600

    Pull out `Threadable::TYPES`

--- app/models/concerns/threadable.rb
+++ app/models/concerns/threadable.rb
@@ -1,6 +1,8 @@
 module Threadable
   extend ActiveSupport::Concern
 
+  TYPES = %w[ Comment Rollup ]
+
   included do
     has_one :thread_entry, as: :threadable
     after_create -> { create_thread_entry! bubble: bubble }
--- app/models/thread_entry.rb
+++ app/models/thread_entry.rb
@@ -1,7 +1,7 @@
 class ThreadEntry < ApplicationRecord
   belongs_to :bubble, touch: true
 
-  delegated_type :threadable, types: %w[ Comment Rollup ], inverse_of: :thread_entry, dependent: :destroy
+  delegated_type :threadable, types: Threadable::TYPES, inverse_of: :thread_entry, dependent: :destroy
 
   scope :chronologically, -> { order created_at: :asc, id: :desc }
 end
