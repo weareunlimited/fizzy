6bd15dd6531461a2b50bcb62ea1fe1569ef78ffb
Jason Zimdars <jz@37signals.com>
2024-12-16 10:42:11 -0600

    Adjust tests to match new behavior

--- test/controllers/boosts_controller_test.rb
+++ test/controllers/boosts_controller_test.rb
@@ -6,8 +6,20 @@ class BoostsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "create" do
+    boost_count = bubbles(:logo).boosts_count
+
     assert_difference "bubbles(:logo).reload.boosts_count", +1 do
-      post bucket_bubble_boosts_url(buckets(:writebook), bubbles(:logo), format: :turbo_stream)
+      post bucket_bubble_boosts_url(buckets(:writebook), bubbles(:logo), params: { boost_count: boost_count }, format: :turbo_stream)
+    end
+
+    assert_turbo_stream action: :update, target: dom_id(bubbles(:logo), :boosts)
+  end
+
+  test "create with value" do
+    boost_count = 10
+
+    assert_changes "bubbles(:logo).reload.boosts_count", to: boost_count do
+      post bucket_bubble_boosts_url(buckets(:writebook), bubbles(:logo), params: { boost_count: boost_count }, format: :turbo_stream)
     end
 
     assert_turbo_stream action: :update, target: dom_id(bubbles(:logo), :boosts)
